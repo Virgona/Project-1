fdf4984a0780250c19684233a24ca73b9ce75cfc
ChristopherGatt <chris.gatt.3@gmail.com>
2022-09-06 21:52:24 +1000

    Added button function and h2 tag

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,20 @@
-
-
 function fetchApi() {
-fetch ('https://api.themoviedb.org/3/discover/movie?api_key=c23913981d1239a82bcee942628971b5&with_genres=35').then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+  fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=c23913981d1239a82bcee942628971b5&with_genres=35"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
 
-fetchApi()
+fetchApi();
+
+$("button").on("click", function (event) {
+  event.preventDefault();
+  var selectedGenre = $("select").val();
+  console.log(selectedGenre);
+  $("#selectedGenre").text(selectedGenre);
+});
